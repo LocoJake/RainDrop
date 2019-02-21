aef06ff6465a15e06b9d591202802f3a475d1fd3
LocoJake <jacobwalters@amdg.rockhursths.edu>
2019-02-21 11:37:09 -0600

    Sprite

--- GameOfCircles/Enemy.py
+++ GameOfCircles/Enemy.py
@@ -1,25 +1,14 @@
-class Enemy:
+from Sprite import Sprite
+        
+class Enemy(Sprite):
     
     speed = 8
-    diameter = 20
-    c = color(0,0,255)
+    diameter = 50
+    c = color(0, 0, 255)
     
-    def __init__(self, x, y, team):
-        self.x = x
-        self.y = y
-        self.team = team
-        
     def move(self):
         self.x += self.speed
         if self.x < 0 or self.x > width:
             self.speed *= -1
         if self.x < 0:
             self.x = width
-        
-    def display(self):
-        fill(self.c)
-        ellipse(self.x, self.y, self.diameter, self.diameter)
-        
-    def animate(self):
-        self.move()
-        self.display()
